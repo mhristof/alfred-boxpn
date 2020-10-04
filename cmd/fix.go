7e5818302d49e6a220aad0503ad159566152126d
Mike Christof <mhristof@gmail.com>
2020-10-04 14:03:51 +0100

    handle multiple results

--- cmd/fix.go
+++ cmd/fix.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -8,6 +9,7 @@ import (
 	"strings"
 
 	"github.com/mhristof/alfred-boxpn/config"
+	"github.com/mhristof/go-country"
 	"github.com/spf13/cobra"
 )
 
@@ -23,15 +25,9 @@ var (
 
 			for _, file := range files {
 				path := filepath.Join(config.Configs, file.Name())
-				f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-				if err != nil {
-					log.Println(err)
-				}
-				defer f.Close()
-				if _, err := f.WriteString("\nauth-user-pass " + config.Auth); err != nil {
-					log.Println(err)
-				}
 
+				//authUserPass(path)
+				flag(file.Name())
 				err = os.Rename(path, strings.ReplaceAll(path, " ", "_"))
 				if err != nil {
 					panic(err)
@@ -41,6 +37,38 @@ var (
 		},
 	}
 )
+
+func flag(file string) {
+	parts := strings.Split(file, "_")
+	name := parts[0]
+
+	fmt.Println(fmt.Sprintf("country: %+v", name))
+
+	c, err := country.FindName(name)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(c.Flag)
+}
+
+func authUserPass(path string) {
+	contents, err := ioutil.ReadFile(path)
+
+	if strings.Contains(string(contents), "auth-user-pass") {
+		fmt.Println("skipping", path)
+		return
+	}
+
+	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println(err)
+	}
+	defer f.Close()
+	if _, err := f.WriteString("\nauth-user-pass " + config.Auth); err != nil {
+		log.Println(err)
+	}
+}
 
 func init() {
 	rootCmd.AddCommand(fixCmd)
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.14
 require (
 	github.com/keybase/go-keychain v0.0.0-20200502122510-cda31fe0c86d
 	github.com/mhristof/go-alfred v1.1.0
+	github.com/mhristof/go-country v0.0.0-20201004124902-e48983e3661c
 	github.com/spf13/cobra v1.0.0
 )
