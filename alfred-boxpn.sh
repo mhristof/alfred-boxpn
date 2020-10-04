c29bd765291d7a27ca8987f338a9e6bc60427f5c
Mike Christof <mhristof@gmail.com>
2020-10-04 15:31:29 +0100

    trust the binary

--- alfred-boxpn.sh
+++ alfred-boxpn.sh
@@ -3,7 +3,7 @@
 
 GOBIN=$(basename $0 .sh)
 xattr "$GOBIN" | grep 'com.apple.quarantine' && {
-    osascript -e 'display dialog "Alfred: Do you want to trust this version of paste ?" buttons {"No", "Yes"}' | grep 'returned:Yes' && xattr -d com.apple.quarantine paste
+    osascript -e 'display dialog "Alfred: Do you want to trust this version of paste ?" buttons {"No", "Yes"}' | grep 'returned:Yes' && xattr -d com.apple.quarantine "$GOBIN"
 }
 
 ./$GOBIN $*
